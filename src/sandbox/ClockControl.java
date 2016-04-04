0f9b1f88db74ecc3669e27a03a53fc3c59be9d09
aah5307 <aah5307@208-NWD43.up.ist.local>
2016-04-04 15:50:44 -0400

    started working on clock

---
+++ src/sandbox/ClockControl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sandbox;
+
+/**
+ *
+ * @author aah5307
+ */
+public class ClockControl {
+    
+    Clock clock;
+    
+    public void speedup(){
+        
+    }
+    
+    public void slowdown(){
+        
+    }
+    
+}
