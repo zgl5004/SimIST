80f2eb82b5521a60ea8b92f9e0e55979b237a312
lmo5113 <lmo5113@208-NWD15.up.ist.local>
2016-04-04 15:31:09 -0400

    working on Options class

--- src/controllers/Options.java
+++ src/controllers/Options.java
@@ -5,16 +5,46 @@
  */
 package controllers;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+
 /**
  *
  * @author aot5238
  */
-public class Options 
+public final class Options extends JPanel
 {
     
-    public Options()
+    public Options(int panelWidth, int panelHeight)
     {
-        
+        super();
+        setSize((panelWidth * (1 / 10)), panelHeight);
+        setPreferredSize(new Dimension(300, 450));
+        setBackground(Color.GREEN);
+        setLayout(new GridBagLayout());
+
     }
+
+    @Override
+    public void setSize(int i, int panelHeight) {
+        throw new UnsupportedOperationException(); 
+    }
+
+  
+    @Override
+    public void setPreferredSize(Dimension dimension) {
+        throw new UnsupportedOperationException(""); 
+    }
+
+    @Override
+    public void setBackground(Color GREEN) {
+        throw new UnsupportedOperationException(""); 
+    }
+
+    private void setLayout(GridBagLayout gridBagLayout) {
+        throw new UnsupportedOperationException(); 
+                }
     
 }
