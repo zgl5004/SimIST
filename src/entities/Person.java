1f374850b35d3197314d23809454c70112a20390
Jon <Jon@client-104-39-92-120.mobility.psu.edu>
2016-04-04 15:45:08 -0400

    created constructor for person class

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -6,6 +6,8 @@
 
 package entities;
 
+import java.util.Random;
+
 /**
  *
  * @author Jon
@@ -16,9 +18,35 @@ public class Person {
     private int stress;
     private int disipline;
     private int attractiveness;
+    private int luck;
     private String gender;
     
     Person(){
+        Random rand = new Random();
+        
+        int intelligence = rand.nextInt(10) + 1;
+        this.intelligence = intelligence;
+        
+        int charisma = rand.nextInt(10) + 1;
+        this.charisma = charisma;
+        
+        int stress = rand.nextInt(10) + 1;
+        this.stress = stress;
+        
+        int disipline = rand.nextInt(10) + 1;
+        this.disipline = disipline;
+        
+        int attractiveness = rand.nextInt(10) + 1;
+        this.attractiveness = attractiveness;
+        
+        int luck = rand.nextInt(10) + 1;
+        this.luck = luck;
+        
+        //gender under construction
+    }
+    
+    public void printAttributes(){
         
     }
+    
 }
