571846790c2ad8a764766b1b65b2d4114cdbff78
amm6946 <amm6946@208-NWD41.up.ist.local>
2016-04-04 15:41:29 -0400

    Added the attributes to the class.
    -A

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -1,7 +1,9 @@
 
 package entities;
 
-public class Person implements MouseListener
-{
+public class Person implements MouseListener {
+    private String fName;
+    private String lName;
+    private int age;
     
 }
