347a719ce7e14706f35eadd12295379cbe103b19
sns5408 <sns5408@208-NWD11.up.ist.local>
2016-04-04 15:33:11 -0400

    Added attributes for artifacts

--- src/entities/Artifacts.java
+++ src/entities/Artifacts.java
@@ -5,10 +5,45 @@
  */
 package entities;
 
+import java.util.ArrayList;
+import javafx.scene.image.Image;
+
 /**
  *
  * @author sns5408
  */
 public class Artifacts {
+    
+   public String ArtifactMaterial; 
+   public String ArtifactType;
+   
+   public boolean isUsable;
+   public boolean isEditable; 
+   public boolean isCollidable; 
+   
+   public int xLocation;
+   public int yLocation; 
+  
+   ArrayList <Image> artifacts;  
+   
+    
+    
+            
+ public void getArtifactType(){
+        //return Type of Artifact ie: chair, computer, painting, plant, table, etc. 
+    }
+    
+    public void getImage(){
+        //return image
+    }
+    public void getLocation(){
+        //return location
+    }
+    
+ 
+    
+    
+    
+    
     
 }
