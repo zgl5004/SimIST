40af7ab6c2fa215ff95eb44f3814ebfdfd08d8a6
antonizoon <sagnessagiel@gmail.com>
2016-04-06 16:17:33 -0400

    Adding a building to collate all floors

--- src/controllers/Unit.java
+++ src/controllers/Unit.java
@@ -2,16 +2,30 @@ package controllers;
 
 /**
  * Interim Unit class, representing a mobile object in the IST building.
- * @author lvw5264s
+ * @author lvw5264
  */
 public class Unit {
     protected String name;
+    protected Floor floor;
     
-    public Unit(String name) {
+    public Unit(String name, Floor floor) {
         this.name = name;
+        this.floor = floor;
     }
     
     public String getName() {
         return this.name;
+    }
+    
+    public Floor getFloor() {
+        return this.floor;
+    }
+    
+    public int getFloorLevel() {
+        return this.floor.getLevel();
+    }
+    
+    public void setFloor(Floor floor) {
+        this.floor = floor;
     }
 }
