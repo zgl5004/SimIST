2b98cfe7426f84df14da174e7747436b258b6358
antonizoon <sagnessagiel@gmail.com>
2016-04-06 15:07:06 -0400

    Create interim Unit and Person classes as pieces to move around

---
+++ src/controllers/Person.java
@@ -0,0 +1,11 @@
+package controllers;
+
+/**
+ * Interim Person class, representing a mobile person, which is also a Unit.
+ * @author lvw5264
+ */
+public class Person extends Unit {
+    public Person(String name) {
+        super(name);
+    }
+}
