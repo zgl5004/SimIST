8eda4f6f010bde55d18ab97b6adecc0e3eeb9880
jensun93 <jensun93@gmail.com>
2016-04-06 15:24:51 -0400

    Add classe: Mover Add Methods

--- src/controllers/Mover.java
+++ src/controllers/Mover.java
@@ -1,7 +1,29 @@
-
 package controllers;
 
+import java.util.ArrayList;
 
 public class Mover {
+
+    protected ArrayList<Floor> floors;
+    
+    public Mover(){
+        ArrayList<Floor> floors = new ArrayList<Floor>();
+    }
+    
+    public void addFloor(Floor f){
+        floors.add(f);
+    }
+    
+    public void removeFloor(Floor f){
+        floors.remove(f);
+    }
+
+    public ArrayList<Floor> getFloors() {
+        return floors;
+    }
+
+    public void setFloors(ArrayList<Floor> floors) {
+        this.floors = floors;
+    }
     
 }
