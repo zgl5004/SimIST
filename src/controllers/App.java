a326c8d7a8c0875330895c96e6cb334fc497bc50
lap5486 <lap5486@208-NWD08.up.ist.local>
2016-04-06 15:43:28 -0400

    basic url usage and reading.

--- src/controllers/App.java
+++ src/controllers/App.java
@@ -1,5 +1,7 @@
 package controllers;
-
+import java.net.*;
+import java.io.*;
+import java.util.*;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,8 +17,26 @@ public class App {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+        {
+        String[] weatherSite;
+        URL weather = new URL("http://us.rd.yahoo.com/dailynews/rss/weather/Country__Country/*https://weather.yahoo.com/country/state/city-2499253/");
+        URLConnection yc = weather.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                                    yc.getInputStream()));
+        String inputLine;
+        
+        while (((inputLine = in.readLine()))!= null) {
+            System.out.println(inputLine);
+            
+            
+            
+        in.close();
+        }
+        
+       
+        } 
+
     }
-    
 }
+
