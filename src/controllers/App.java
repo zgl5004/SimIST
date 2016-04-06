52e8bed2102863dd6b46f63c0ee9ce56c225ef49
matthewtucker <matthewtucker@client-130-203-90-194.mobility.psu.edu>
2016-04-06 15:49:16 -0400

    Updated floor and added inner room and player classes to test functionality

--- src/controllers/App.java
+++ src/controllers/App.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import javax.swing.JFrame;
+import views.*;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,7 +19,13 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        GameFrame frame1 = new GameFrame("Frame");
+        frame1.setVisible(true);
+       Floor Floor1 = new Floor("Floor 1"); 
+       Floor1.setVisible(true);
+        frame1.add(Floor1);
+        frame1.revalidate();
+        frame1.repaint();
     }
     
 }
--- src/views/Floor.java
+++ src/views/Floor.java
@@ -7,8 +7,13 @@ package views;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -16,16 +21,24 @@ import javax.swing.Timer;
  *
  * @author matthewtucker
  */
-public class Floor extends JPanel implements ActionListener {
+public class Floor extends JPanel implements ActionListener, KeyListener {
    private String floorName;
    private Image floorImage;
    private Timer refreshTimer;
+   private ArrayList<Room> rooms;
+   private player player1;
    
-   public Floor(String floorName, Image floorImage){
-       refreshTimer = new Timer (750, this);
+   public Floor(String floorName){
+       refreshTimer = new Timer (50, this);
        refreshTimer.start();
        this.floorName = floorName;
-       this.floorImage = floorImage;
+       //this.floorImage = floorImage;
+       rooms = new ArrayList<>();
+       rooms.add(new Room(3, 50,50));
+       player1 = new player();
+       this.addKeyListener(this);
+       setFocusable(true);
+        requestFocusInWindow();
    }
    
    
@@ -42,7 +55,18 @@ public class Floor extends JPanel implements ActionListener {
    }
    
    public void paintComponent(Graphics g){
+       super.paintComponent(g);
+        g.clearRect(0, 0, this.getWidth(), this.getHeight());
+       for(int i = 0; i < rooms.size();i++){
+           rooms.get(i).paintComponent(g);
+       }
+       player1.paintComponent(g);
        
+       for(int i = 0; i<rooms.size();i++){
+          if(player1.intersects(rooms.get(i))){
+              JOptionPane.showConfirmDialog(this,"This is room number"+rooms.get(i).getNum()+"Do you want to enter?");
+          }
+       }
    }
    
    public void actionPerformed(ActionEvent e){
@@ -50,6 +74,89 @@ public class Floor extends JPanel implements ActionListener {
        
        if(o == refreshTimer){
            this.repaint();
+       }
+   }  
+    public void keyPressed(KeyEvent e){
+       if(e.getKeyCode()== KeyEvent.VK_LEFT){
+        player1.setDx(-20);
+       
+        System.out.println("Typed");
+       }
+       
+       else if(e.getKeyCode() ==KeyEvent.VK_RIGHT){
+           player1.setDx(20);
+       }
+    }   
+    
+    public void keyReleased(KeyEvent e){
+        player1.setDx(0);
+    }
+    public void keyTyped(KeyEvent e){
+        
+    }
+    
+    public void addPlayerToFloor(){
+        
+    }
+   
+   
+    
+    
+    
+    
+    
+    private class player extends Rectangle{
+         private int dx;
+        public player(){
+            this.dx = 0;
+            
+            this.setBounds(100, 50, 30, 30);
+        }
+        
+        public void move(){
+            this.x+=dx;
+        }
+        
+        public void setDx(int dx){
+            this.dx = dx;
+        }
+        
+        public void paintComponent(Graphics g){
+            move();
+            g.drawRect(x, y, 30, 30);
+        }
+    }
+   
+   
+   
+   
+   
+   
+   private class Room extends Rectangle{
+       private int roomNum;
+       private int floor;
+       private int capacity;
+       private int xLocation;
+       private int yLocation;
+       //private JPanel roomView;
+       
+       public Room(int roomNum, int x, int y){
+           this.roomNum = roomNum;
+           //this.roomView = roomView;
+           this.xLocation = x;
+           this.yLocation = y;
+           this.capacity = 50;
+           this.floor = 1;
+           
+           this.setBounds(this.xLocation, this.yLocation, 30, 30);
+       }
+       
+       public void paintComponent(Graphics g){
+           g.drawRect(x, y, 30, 30);
+       }
+       
+       public int getNum(){
+           return this.roomNum;
        }
    }
 }
