4a4e34535ae931d2e3857a39e6a54efb6f818be0
vqm5108 <vqm5108@208-NWD40.up.ist.local>
2016-04-06 15:58:37 -0400

    Made an ArrayList to generate names for the people that are in the game.

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -1,22 +1,35 @@
 
 package entities;
 
+import java.util.ArrayList;
+
 public abstract class Person {
     private String fName;
     private String lName;
     private int age;
+    private ArrayList<String> names = new ArrayList<String>();
+   
     
-    public Person(String fName, String lName, int age)
+    public Person(String fName, String lName, int age, ArrayList<String> names)
     {
         this.fName = fName;
         this.lName = lName;
         this.age = age;
+        this.names = names;
+     names.add("Aaron");
+     names.add("Vince");
+     names.add("Levi");
+     names.add("Sarah");
+     names.add("Emily");
+     names.add("McFarland");
+     names.add("Maimone");
+     names.add("Smith");
     }
-    
     public void getInfo()
     {
         System.out.println("First name: " + fName + "Last name: " + lName + "Age: " + age);
     }
+    
     
     
 }
