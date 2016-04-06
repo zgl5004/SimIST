07b7aa71552c7ae506fdf70ac841cc86ec428e63
jensun93 <jensun93@gmail.com>
2016-04-06 15:11:03 -0400

    Add classe: Floor

---
+++ src/controllers/Floors.java
@@ -0,0 +1,19 @@
+
+package controllers;
+
+import java.util.ArrayList;
+
+
+public class Floors {
+    
+   ArrayList <Unit> unitList = new ArrayList <Unit>();
+   
+   
+   public void addUnit(Unit u){
+       
+       
+   }
+    
+    
+    
+}
