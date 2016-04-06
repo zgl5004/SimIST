9c11cd43d312dd05ed2cdb039a54b47827477626
antonizoon <sagnessagiel@gmail.com>
2016-04-06 15:31:17 -0400

    Create a JUnit test for Unit class.

--- src/controllers/App.java
+++ src/controllers/App.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,7 +18,15 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create an example unit
+        Unit unit = new Unit("Example Unit");
+        Person person = new Person("Example Person");
+        System.out.println(unit.getName());
+        System.out.println(person.getName());
+        
+        // create a test floor class
+        //Floor floor = new Floor();
+        //ArrayList<Unit> pplOnFlr = floor.getUnitList();
     }
     
 }
