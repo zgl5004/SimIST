5163edd8997d439163508eb4da2498a1182786bc
antonizoon <sagnessagiel@gmail.com>
2016-04-06 15:08:19 -0400

    Create interim Unit classes as parent of Person

---
+++ src/controllers/Unit.java
@@ -0,0 +1,17 @@
+package controllers;
+
+/**
+ * Interim Unit class, representing a mobile object in the IST building.
+ * @author lvw5264
+ */
+public class Unit {
+    protected String name;
+    
+    public Unit(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+}
