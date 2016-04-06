be0a98be0b208da6fd2f1a757548e7807b3ce77f
antonizoon <sagnessagiel@gmail.com>
2016-04-06 15:50:04 -0400

    Merging from upstream

--- src/controllers/App.java
+++ src/controllers/App.java
@@ -19,14 +19,22 @@ public class App {
      */
     public static void main(String[] args) {
         // create an example unit
-        Unit unit = new Unit("Example Unit");
+        Unit item = new Unit("Example Unit");
         Person person = new Person("Example Person");
-        System.out.println(unit.getName());
+        System.out.println(item.getName());
         System.out.println(person.getName());
         
         // create a test floor class
-        //Floor floor = new Floor();
-        //ArrayList<Unit> pplOnFlr = floor.getUnitList();
+        Floor floor = new Floor();
+        ArrayList<Unit> pplOnFlr = floor.getUnitList();
+        floor.addUnit(new Person("Guy 1"));
+        floor.addUnit(new Person("Girl 1"));
+                
+        // display all elements in the arraylist
+        for (Unit unit : floor.getUnitList()) {
+            System.out.println(unit.getName());
+        }
+        
     }
     
 }
