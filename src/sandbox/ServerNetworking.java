fdf01e47822aafabd86f4aceeea874a989f2c920
kjrya <kjrya@104.39.127.8>
2016-04-06 15:18:20 -0400

    added things

--- src/sandbox/ServerNetworking.java
+++ src/sandbox/ServerNetworking.java
@@ -5,10 +5,31 @@
  */
 package sandbox;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author kjrya
  */
-public class ServerNetworking {
-    
+public class ServerNetworking 
+{
+    public static void main(String[] args) throws Exception
+    {
+        String incomingData;
+        String capIncomingData;
+        ServerSocket welcomeSocket = new ServerSocket(5555);
+        
+        while(true){
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader incoming = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outgoing = new DataOutputStream(connectionSocket.getOutputStream());
+            incomingData = incoming.readLine();
+            capIncomingData = incomingData.toUpperCase() + '\n';
+            outgoing.writeBytes(capIncomingData);
+        }
+    }
 }
