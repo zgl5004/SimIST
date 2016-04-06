697364a58d956f640519a2cf53e1e2a76a053999
lmo5113 <lmo5113@208-NWD01.up.ist.local>
2016-04-06 15:15:30 -0400

    Luke and Alex

--- src/controllers/Options.java
+++ src/controllers/Options.java
@@ -8,14 +8,22 @@ package controllers;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
  *
- * @author aot5238
+ * @author aot5238 and lmo5113
+ *
  */
 public final class Options extends JPanel
 {
+     
+     int panelWidth;
+     int panelHeight;
+     private boolean pauseOption = false; 
+     private JButton start; 
+     private JButton pause; 
     
     public Options(int panelWidth, int panelHeight)
     {
@@ -26,12 +34,32 @@ public final class Options extends JPanel
         setLayout(new GridBagLayout());
 
     }
+    
+    public void addComponents(){
+        start = new JButton("Start");
+        pause = new JButton("Pause");
+    }
+    
+    public void setStart(JButton start) {
+        this.start = start;
+    }
+    
+     public JButton getStart() {
+        return start;
+    }
+    
+      public void setPauseOption(JButton pause) {
+        this.pauseOption = pauseOption; 
+    }
+      
+       public boolean getPauseOption() {
+        return pauseOption;
+    }
 
     @Override
     public void setSize(int i, int panelHeight) {
         throw new UnsupportedOperationException(); 
     }
-
   
     @Override
     public void setPreferredSize(Dimension dimension) {
@@ -46,5 +74,5 @@ public final class Options extends JPanel
     private void setLayout(GridBagLayout gridBagLayout) {
         throw new UnsupportedOperationException(); 
                 }
-    
+
 }
